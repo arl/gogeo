281a7f15f26878d065d26c80029e827b628f5c86
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-11-10 02:52:00 +0100

    f64: replace FloatEqual/Threshold with Approx/Epsilon

---
+++ f64/approx.go
@@ -0,0 +1,22 @@
+package f64
+
+import "math"
+
+var (
+	epsilon64 float64
+)
+
+func init() {
+	epsilon64 = math.Nextafter64(1, 2) - 1
+}
+
+// Approx returns true if x ~= y
+func Approx(x, y float64) bool {
+	eps := epsilon64 * 100
+	return math.Abs(x-y) < eps*(1.0+math.Max(math.Abs(x), math.Abs(y)))
+}
+
+// ApproxEpsilon returns true if x ~= y, using provided epsilon value.
+func ApproxEpsilon(x, y float64, eps float64) bool {
+	return math.Abs(x-y) < eps*(1.0+math.Max(math.Abs(x), math.Abs(y)))
+}
--- f64/floatutils.go
+++ f64/floatutils.go
@@ -10,69 +10,6 @@ import (
 	"math"
 )
 
-// Epsilon is some tiny value that determines how precisely equal we want our floats to be
-// This is exported and left as a variable in case you want to change the default threshold for the
-// purposes of certain methods (e.g. Unproject uses the default epsilon when determining
-// if the determinant is "close enough" to zero to mean there's no inverse).
-//
-// This is, obviously, not mutex protected so be **absolutely sure** that no functions using Epsilon
-// are being executed when you change this.
-var Epsilon = 1e-10
-
-// FloatEqual is a safe utility function to compare floats.
-// It's Taken from http://floating-point-gui.de/errors/comparison/
-//
-// It is slightly altered to not call Abs when not needed.
-func FloatEqual(a, b float64) bool {
-	return FloatEqualThreshold(a, b, Epsilon)
-}
-
-// FloatEqualFunc is a utility closure that will generate a function that
-// always approximately compares floats like FloatEqualThreshold with a different
-// threshold.
-func FloatEqualFunc(epsilon float64) func(float64, float64) bool {
-	return func(a, b float64) bool {
-		return FloatEqualThreshold(a, b, epsilon)
-	}
-}
-
-var (
-	// MinNormal is the minimum positive normal 1 / 2**(127 - 1)
-	MinNormal = 1.1754943508222875e-38
-	// MinValue is next smallest float after 0
-	MinValue = math.SmallestNonzeroFloat64
-	// MaxValue is the maximal value representable with a float64
-	MaxValue = math.MaxFloat64
-
-	// InfPos represents the positive infinity
-	InfPos = math.Inf(1)
-	// InfNeg represents the negative infinity
-	InfNeg = math.Inf(-1)
-	// NaN represents a not-a-number
-	NaN = math.NaN()
-)
-
-// FloatEqualThreshold is a utility function to compare floats.
-// It's Taken from http://floating-point-gui.de/errors/comparison/
-//
-// It is slightly altered to not call Abs when not needed.
-//
-// This differs from FloatEqual in that it lets you pass in your comparison threshold, so that you can adjust the comparison value to your specific needs
-func FloatEqualThreshold(a, b, epsilon float64) bool {
-	if a == b { // Handles the case of inf or shortcuts the loop when no significant error has accumulated
-		return true
-	}
-
-	diff := math.Abs(a - b)
-
-	if a == 0 || b == 0 || diff < MinNormal { // If a or b are 0 or both are extremely close to it
-		return diff < epsilon*MinNormal
-	}
-	// Else compare difference
-	absA, absB := math.Abs(a), math.Abs(b)
-	return diff/math.Min((absA+absB), MaxValue) < epsilon
-}
-
 // Clamp takes in a value and two thresholds. If the value is smaller than the low
 // threshold, it returns the low threshold. If it's bigger than the high threshold
 // it returns the high threshold. Otherwise it returns the value.
