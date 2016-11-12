0aeef9032412d8e5ab0bc4b8aa2b7b95cd8204fa
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-11-12 19:55:28 +0100

    f32/d2: Ray uses Vec2

--- f32/d2/ray.go
+++ f32/d2/ray.go
@@ -10,51 +10,51 @@ import (
 
 // ray, or line
 type Ray struct {
-	o    Vec // origin
-	v    Vec // unit direction vector
-	invv Vec // inv of unit direction vector
+	o    Vec2 // origin
+	v    Vec2 // unit direction vector
+	invv Vec2 // inv of unit direction vector
 }
 
-func NewRay(o, v Vec) Ray {
-	//v = v.Normalize()
+func NewRay(o, v Vec2) Ray {
+	v.Normalize()
 	return Ray{
 		o:    o,
 		v:    v,
-		invv: Vec{1.0 / v.X, 1.0 / v.Y},
+		invv: NewVec2XY(1.0/v[0], 1.0/v[1]),
 	}
 }
 
-func (r Ray) Origin() Vec {
+func (r Ray) Origin() Vec2 {
 	return r.o
 }
 
-func (r Ray) Direction() Vec {
+func (r Ray) Direction() Vec2 {
 	return r.v
 }
 
 func (r Ray) IntersectRect(b Rectangle) bool {
-	t1 := (b.Min.X - r.o.X) * r.invv.X
-	t2 := (b.Max.X - r.o.X) * r.invv.X
+	t1 := (b.Min[0] - r.o[0]) * r.invv[0]
+	t2 := (b.Max[0] - r.o[0]) * r.invv[0]
 
 	tmin := math32.Min(t1, t2)
 	tmax := math32.Max(t1, t2)
 
 	// x
-	t1 = (b.Min.X - r.o.X) * r.invv.X
-	t2 = (b.Max.X - r.o.X) * r.invv.X
+	t1 = (b.Min[0] - r.o[0]) * r.invv[0]
+	t2 = (b.Max[0] - r.o[0]) * r.invv[0]
 	tmin = math32.Max(tmin, math32.Min(t1, t2))
 	tmax = math32.Min(tmax, math32.Max(t1, t2))
 
 	// y
-	t1 = (b.Min.Y - r.o.Y) * r.invv.Y
-	t2 = (b.Max.Y - r.o.Y) * r.invv.Y
+	t1 = (b.Min[1] - r.o[1]) * r.invv[1]
+	t2 = (b.Max[1] - r.o[1]) * r.invv[1]
 	tmin = math32.Max(tmin, math32.Min(t1, t2))
 	tmax = math32.Min(tmax, math32.Max(t1, t2))
 
 	return tmax >= math32.Max(tmin, 0.0)
 }
 
-// String returns a string representation of r like with (o:Vec,v:Vec).
+// String returns a string representation of r like with (o:Vec2,v:Vec2).
 func (r Ray) String() string {
 	return fmt.Sprintf("(o:%v,v:%v)", r.o, r.v)
 }
--- f32/d2/ray_test.go
+++ f32/d2/ray_test.go
@@ -9,27 +9,27 @@ func TestRay(t *testing.T) {
 		r    Ray
 		want bool
 	}{
-		{RectWH(1, -1, 1, 1), NewRay(Vec{0, 0}, Vec{1, -0.9}), true},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{0, 0}, Vec{1, -1}), true},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{0, 0}, Vec{1, -1.2}), false},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{0, 0}, Vec{1, -0.001}), true},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{0, 0}, Vec{1, +0.001}), false},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{0, 1}, Vec{1, -1.999}), true},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{0, 0}, Vec2{1, -0.9}), true},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{0, 0}, Vec2{1, -1}), true},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{0, 0}, Vec2{1, -1.2}), false},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{0, 0}, Vec2{1, -0.001}), true},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{0, 0}, Vec2{1, +0.001}), false},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{0, 1}, Vec2{1, -1.999}), true},
 
 		// vertical (inf slope)
-		{RectWH(1, -1, 1, 1), NewRay(Vec{1.5, 1}, Vec{0, -1}), true},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{0.5, 1}, Vec{0, -1}), false},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{2.5, 1}, Vec{0, -1}), false},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{1.5, -1.5}, Vec{0, 1}), true},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{1.5, -1.5}, Vec{0, -1}), false},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{1.5, 1}, Vec2{0, -1}), true},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{0.5, 1}, Vec2{0, -1}), false},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{2.5, 1}, Vec2{0, -1}), false},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{1.5, -1.5}, Vec2{0, 1}), true},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{1.5, -1.5}, Vec2{0, -1}), false},
 
 		// horizontal (slope = 0)
-		{RectWH(1, -1, 1, 1), NewRay(Vec{0, -0.5}, Vec{1, 0}), true},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{0, 0.5}, Vec{1, 0}), false},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{0, -1.5}, Vec{1, 0}), false},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{0, -0.5}, Vec{-1, 0}), false},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{3, -0.5}, Vec{1, 0}), false},
-		{RectWH(1, -1, 1, 1), NewRay(Vec{3, -0.5}, Vec{-1, 0}), true},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{0, -0.5}, Vec2{1, 0}), true},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{0, 0.5}, Vec2{1, 0}), false},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{0, -1.5}, Vec2{1, 0}), false},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{0, -0.5}, Vec2{-1, 0}), false},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{3, -0.5}, Vec2{1, 0}), false},
+		{RectWH(1, -1, 1, 1), NewRay(Vec2{3, -0.5}, Vec2{-1, 0}), true},
 	}
 
 	for _, tt := range tests {
